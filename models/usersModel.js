c8e867d916e2e068f504629f34f55c7fc8010ce7
coding_monster <42187068+anudeep4952@users.noreply.github.com>
2020-06-04 14:56:21 +0530

    Update usersModel.js

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const jwt=require('jsonwebtoken')
+
+require('dotenv').config()
 
 
 const usersSchema = new mongoose.Schema({
@@ -7,19 +10,35 @@ const usersSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+
     mailId: {
         type: String,
         required: true
     },
-    password: {
-        type: String,
-        required: true,
+    token:{
+        type:String,
     },
-    isActive:{
-        type:Boolean,
-        default:false
+    profilePicUrl:{
+        type:String,
+        required:true
     }
-
 })
+
+
+
+
+usersSchema.methods.generateJWT=async function(){
+    try{
+    const user =  this
+    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET_KEY,
+        { expiresIn: '1h',})
+    user.token=token 
+    await user.save()
+    return token
+    }catch(err){
+       return err
+    }
+}
+
 
 module.exports = mongoose.model('userSchema',usersSchema)
